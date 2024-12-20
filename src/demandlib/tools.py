29aa4ce83f8f00a9ec2e3842145771de431c9cbc
Joris Nettelstroth <joris.nettelstroth@stw.de>
2024-12-20 11:59:28 +0100

    Fix more future warnings from pandas 2.2

--- src/demandlib/tools.py
+++ src/demandlib/tools.py
@@ -35,13 +35,12 @@ def add_weekdays2df(time_df, holidays=None, holiday_is_sunday=False):
     if holidays is not None:
         if isinstance(holidays, dict):
             holidays = list(holidays.keys())
-        time_df["weekday"].mask(
+        time_df["weekday"] = time_df["weekday"].mask(
             pd.to_datetime(time_df["date"]).isin(pd.to_datetime(holidays)),
             0,
-            inplace=True,
         )
 
     if holiday_is_sunday:
-        time_df.weekday.mask(time_df.weekday == 0, 7, inplace=True)
+        time_df["weekday"] = time_df["weekday"].mask(time_df.weekday == 0, 7)
 
     return time_df
